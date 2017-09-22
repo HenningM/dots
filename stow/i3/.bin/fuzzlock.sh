c4db54e9552e2d7f86fd31d02c6d610b34d8e98b
Henning Morud <h.morud@sportradar.com>
2017-09-22 12:24:26 +0200

    i3-lock: Don't unmute on unlock if muted before lock

--- stow/i3/.bin/fuzzlock.sh
+++ stow/i3/.bin/fuzzlock.sh
@@ -9,10 +9,20 @@ scrot /tmp/screen_locked.png
 # Blur it
 mogrify -grayscale Rec709Luma -blur 0x10 /tmp/screen_locked.png
 
+# Commands to run on unlock
+unlock_cmds=""
+
 # Lock screen displaying this image.
 # Mute the audio output while screen is locked.
-(i3lock -n -i /tmp/screen_locked.png; amixer set Master unmute)&
-amixer set Master mute
+audio_status=`amixer sget Master | grep "\[off\]" || true`
+unmute_cmd="amixer -q set Master unmute"
+if [ "$audio_status" = "" ]; then
+  unlock_cmds="$unlock_cmds$unmute_cmd;"
+  amixer -q set Master mute
+fi
+
+# Run i3lock & any unlock
+(i3lock -n -i /tmp/screen_locked.png; $unlock_cmds)&
 
 # Turn the screen off after a delay.
 sleep 900; pgrep i3lock && xset dpms force off
